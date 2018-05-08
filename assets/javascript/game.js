c69a2200b8aa80d4c7c103bc42dc0e8ed9cd03d5
jfenneyj <jfenney@leadtomarket.com>
2018-05-08 08:06:57 -0700

    update file1

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// variables
+//////////////////////////////////
+var guessWord = ["frizzy", "quacky", "jockey", "zombie", "object"];
+var wins = 0;
+var loss = 0;
+var wrongLetter = [];
+var guessesLeft = 9;
+var underScores = [];
+var userGuesses = [];
+
+
+// Function
+//////////////////////////////////////
+
+
+// Randomly call words from array
+var randomWord = guessWord[Math.floor(Math.random() * guessWord.length)];
+console.log(randomWord);
+
+  for(var i = 0; i < randomWord.length; i++){
+  
+  }
