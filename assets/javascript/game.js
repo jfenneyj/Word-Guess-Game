b2e3eedfdc298fd553e53a94d4a217a41788ce37
jfenneyj <jfenney@leadtomarket.com>
2018-05-08 14:26:42 -0700

    update assets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,8 @@ var guessWord = ["frizzy", "quacky", "jockey", "zombie", "object"];
 var wins = 0;
 var loss = 0;
 var wrongLetter = [];
-var guessesLeft = 9;
-var underScores = [];
+var attemptsLeft = 10;
+var display = [arrLength];
 var userGuesses = [];
 
 
@@ -18,5 +18,20 @@ var randomWord = guessWord[Math.floor(Math.random() * guessWord.length)];
 console.log(randomWord);
 
   for(var i = 0; i < randomWord.length; i++){
-  
+  // split array into separate charactors
   }
+
+  // input from the user
+
+
+
+// Display on to screen underscore and replace with userGuess letters
+
+
+// If letter matches replace with letter and remove underscore
+
+
+// after 10 attempts user losses and new word is implemented/or starts over
+
+
+// if losses add 1 to loss score if wins add 1 to wins score
